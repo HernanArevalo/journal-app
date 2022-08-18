54d80ee85bb39e1dbef5e5d6cafe0414e0834249
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-08-18 19:05:37 -0300

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,14 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Velit aute est ullamco fugiat occaecat quis non cillum aute sit irure. Aute tempor ullamco nulla adipisicing minim tempor nisi. Veniam ea anim esse labore adipisicing reprehenderit nulla aliqua labore. Esse anim ipsum culpa do incididunt esse cillum in labore do commodo ex. Lorem enim nulla mollit laboris deserunt eu mollit.</Typography>
+      {/* <Typography>Velit aute est ullamco fugiat occaecat quis non cillum aute sit irure. Aute tempor ullamco nulla adipisicing minim tempor nisi. Veniam ea anim esse labore adipisicing reprehenderit nulla aliqua labore. Esse anim ipsum culpa do incididunt esse cillum in labore do commodo ex. Lorem enim nulla mollit laboris deserunt eu mollit.</Typography> */}
+      
+      <NothingSelectedView />
+
 
     </JournalLayout>
   )
