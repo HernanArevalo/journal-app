bc7468f2025aa5c25f1acc65adc9966ecef09df0
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-08-18 12:04:32 -0300

    journal

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,12 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+
+      <Typography>Velit aute est ullamco fugiat occaecat quis non cillum aute sit irure. Aute tempor ullamco nulla adipisicing minim tempor nisi. Veniam ea anim esse labore adipisicing reprehenderit nulla aliqua labore. Esse anim ipsum culpa do incididunt esse cillum in labore do commodo ex. Lorem enim nulla mollit laboris deserunt eu mollit.</Typography>
+
+    </JournalLayout>
   )
 }
