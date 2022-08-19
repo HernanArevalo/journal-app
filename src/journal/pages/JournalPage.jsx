35d207b5e6d24d079eb85b26e2eedd98b6f45af4
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-08-18 23:47:07 -0300

    JournalPage style

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
@@ -8,7 +10,23 @@ export const JournalPage = () => {
       {/* <Typography>Velit aute est ullamco fugiat occaecat quis non cillum aute sit irure. Aute tempor ullamco nulla adipisicing minim tempor nisi. Veniam ea anim esse labore adipisicing reprehenderit nulla aliqua labore. Esse anim ipsum culpa do incididunt esse cillum in labore do commodo ex. Lorem enim nulla mollit laboris deserunt eu mollit.</Typography> */}
       
       <NothingSelectedView />
+      {/* <NoteView /> */}
 
+
+      <IconButton
+        size='large'
+        sx={{ 
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+
+         }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
 
     </JournalLayout>
   )
