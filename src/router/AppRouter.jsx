f5c28472a3013553b379b18a2343db75019c590a
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-09-07 21:26:49 -0300

    keep logged

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,45 @@
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes, } from 'react-router-dom'
+import { FirebaseAuth } from '../firebase/config'
 
+import { login, logout } from '../store/auth'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
+
 import { JournalPage } from '../journal/pages/JournalPage'
+import { CheckingAuth } from '../ui'
+
 
 export const AppRouter = () => {
+
+  const { status } = useSelector( state => state.auth );
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    
+    onAuthStateChanged( FirebaseAuth, async( user ) =>{
+      
+      if ( !user )return dispatch( logout() );
+      
+      const { uid, displayName, photoURL, email } = user
+      dispatch( login({ uid, displayName, photoURL, email }) );
+
+      
+
+    });
+  
+
+
+  }, [  ]);
+  
+
+
+
+  if (status === 'checking'){
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
 
