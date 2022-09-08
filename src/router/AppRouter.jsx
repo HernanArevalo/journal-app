369d5444283757ea674dd745d4cea28dd2a6a6e7
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-09-08 20:36:40 -0300

    keep logged

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Route, Routes, } from 'react-router-dom'
+import { Navigate, Route, Routes, } from 'react-router-dom'
 import { FirebaseAuth } from '../firebase/config'
 
 import { login, logout } from '../store/auth'
@@ -22,32 +22,33 @@ export const AppRouter = () => {
       
       if ( !user )return dispatch( logout() );
       
-      const { uid, displayName, photoURL, email } = user
+      const { uid, displayName, photoURL, email } = user;
       dispatch( login({ uid, displayName, photoURL, email }) );
-
-      
 
     });
   
-
-
-  }, [  ]);
+  }, []);
   
-
-
-
   if (status === 'checking'){
     return <CheckingAuth />
   }
 
   return (
     <Routes>
+        {
+          (status === 'authenticated')
+          ? <Route path="/*" element={ <JournalPage /> }/>
+          : <Route path="/auth/*" element={ <AuthRoutes /> }/>
+
+        }
+
+        <Route path='/*' element={ <Navigate to='/auth/login' /> } />
 
         {/* Login y Registro */}
-        <Route path="/auth/*" element={ <AuthRoutes /> }/>
+        {/* <Route path="/auth/*" element={ <AuthRoutes /> }/> */}
 
         {/* JournalApp */}
-        <Route path="/*" element={ <JournalPage /> }/>
+        {/* <Route path="/*" element={ <JournalPage /> }/> */}
 
     </Routes>
   )
