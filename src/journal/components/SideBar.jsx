d5b306a98305c0687e99efe136e44f386503247e
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-09-19 20:38:23 -0300

    SideBar items

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,11 @@
-import { TurnedInNot } from "@mui/icons-material"
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { Box, Divider, Drawer, List, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./"
 
 export const SideBar = ({ drawerWidth = 240 }) => {
 
     const { displayName } = useSelector( state => state.auth )
+    const { notes } = useSelector( state => state.journal )
 
   return (
     <Box
@@ -28,25 +29,11 @@ export const SideBar = ({ drawerWidth = 240 }) => {
 
             <Divider />
 
-            <List     className="animate__animated animate__fadeInLeft"
->
+            <List className="animate__animated animate__fadeInLeft">
                 {
-                    ['Enero','Febrero','Marzo','Abril'].map(text=>(
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={ text }/>
-                                    <ListItemText secondary={ 'Officia minim fugiat minim mollit laborum.' }/>
-
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
-
+                    notes.map( note =>(
+                        <SideBarItem key={ note.id } { ...note } />
                     ))
-
                 }
 
             </List>
