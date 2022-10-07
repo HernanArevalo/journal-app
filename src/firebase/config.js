9dfdc94384fb4d82ff9e39ab35d23cf6348e2685
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-10-06 22:12:19 -0300

    fixing bug: reading map in ImageGallery

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore/lite'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,8 +14,7 @@ const firebaseConfig = {
   messagingSenderId: "605210604913",
   appId: "1:605210604913:web:24134da79c9c9cf329f2d0"
 };
-
 // Initialize Firebase
-export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseApp  = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth( FirebaseApp );
-export const FirebaseDB = getFirestore( FirebaseApp )
+export const FirebaseDB   = getFirestore( FirebaseApp );
